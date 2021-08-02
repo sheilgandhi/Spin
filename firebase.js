67b3cd36aa665a06268f6f1081d92188bc133398
Sheil Gandhi <gandhishei@ecs.vuw.ac.nz>
2021-08-02 13:39:46 +1200

    Firebase Config Added

---
+++ firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDyKbTGAW9xkyv3gmygmBGd4WmSN-q6J84",
+    authDomain: "spin-dd9be.firebaseapp.com",
+    projectId: "spin-dd9be",
+    storageBucket: "spin-dd9be.appspot.com",
+    messagingSenderId: "974354802314",
+    appId: "1:974354802314:web:0ae84f8502380fd7102f73"
+  };
