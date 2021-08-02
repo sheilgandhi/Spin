df71ce2df51ae030280550e88fe145051c3d7217
Sheil Gandhi <gandhishei@ecs.vuw.ac.nz>
2021-08-02 14:14:41 +1200

    React Navigation Added

--- App.js
+++ App.js
@@ -1,13 +1,28 @@
+import 'react-native-gesture-handler';
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Login from './screens/Login';
+
+const Stack = createNativeStackNavigator();
+
+const globalScreenOptions = {
+  headerStyle: { backgroundColor: "#e3337d" },
+  headerTitleStyle: { color: "white" },
+  headerTintColor: "white",
+}
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    // NavigationContainer stacks screens
+    <NavigationContainer> 
       <StatusBar style="auto" />
-    </View>
+      <Stack.Navigator  screenOptions={globalScreenOptions}>
+        <Stack.Screen name='Login' component={Login} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
