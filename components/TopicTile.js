76435223d6d81aa67d3cac29462783ce5f73cfa6
Sheil Gandhi <gandhishei@ecs.vuw.ac.nz>
2021-08-20 13:43:57 +1200

    Template Component Started

---
+++ components/TopicTile.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+import { ListItem, Avatar } from 'react-native-elements'
+
+const TopicTile = ({ id, chatName, enterChat }) => {
+    return (
+        <ListItem>
+            <Avatar rounded source={{ uri: "https://cencup.com/wp-content/uploads/2019/07/avatar-placeholder.png" }}/>
+
+            <ListItem.Content>
+                <ListItem.Title style={{ fontWeight: "800" }}>Youtube</ListItem.Title>
+                <ListItem.Subtitle numberOfLines={1} ellipsizeMode="tail"> {/* Truncates Text to 1 line */}
+                    Youtube                
+                </ListItem.Subtitle>
+            </ListItem.Content>
+        </ListItem>
+    )
+}
+
+export default TopicTile
+
+const styles = StyleSheet.create({})
