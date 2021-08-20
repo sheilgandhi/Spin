0f731f2424fb9667181475346812524ccac4d404
Sheil Gandhi <gandhishei@ecs.vuw.ac.nz>
2021-08-20 17:26:47 +1200

    Expo crashing please revert to previous commit

--- screens/ChatScreen.js
+++ screens/ChatScreen.js
@@ -1,6 +1,8 @@
 import React, { useLayoutEffect } from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, SafeAreaView, TouchableOpacity, Platform } from 'react-native'
+import { AntDesign, FontAwesome, Ionicons } from "@expo/vector-icons"
 import { Avatar } from 'react-native-elements'
+import { StatusBar } from 'expo-status-bar'
 
 const ChatScreen = ({ navigation, route }) => {
 
@@ -11,17 +13,24 @@ const ChatScreen = ({ navigation, route }) => {
             headerBackTitleVisible: false,
             headerTitle: () => (
                 <View style={{ flexDirection: "row", alignItems: "center" }}>
-                    <Avatar rounded />
-                    <Text>{route.params.chatName}</Text>
+                    <Avatar rounded source={{ uri: `https://cencup.com/wp-content/uploads/2019/07/avatar-placeholder.png`}} />
+                    <Text style={{ color: "white", marginLeft: 10, fontWeight: "700", fontSize: "18" }}>{route.params.chatName}</Text>
+                </View>
+            ),
+            headerRight: () => (
+                <View style={{ flexDirection: 'row', justifyContent: "flex-end", width: 80, marginRight: 20 }}>
+                    <TouchableOpacity>
+                        <AntDesign name="infocirlceo" size={24} color="white" />
+                    </TouchableOpacity>
                 </View>
             )
         })
     }, [])
 
     return (
-        <View>
-            <Text>{route.params.chatName}</Text>
-        </View>
+        <SafeAreaView style={{ flex: 1, backgroundColor: "white" }}>
+            <StatusBar style="light" />
+        </SafeAreaView>
     )
 }
 
