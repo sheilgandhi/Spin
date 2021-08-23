d1e8acba9919da0e86d1275791ffaa7733b4228d
Sheil Gandhi <gandhishei@ecs.vuw.ac.nz>
2021-08-23 16:52:29 +1200

    TopicTile Updated with Firebase dynamic information

--- components/TopicTile.js
+++ components/TopicTile.js
@@ -1,16 +1,32 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { StyleSheet, Text, View } from 'react-native'
 import { ListItem, Avatar } from 'react-native-elements'
+import { db } from '../firebase';
 
 const TopicTile = ({ id, chatName, enterChat }) => {
+    const [messages, setMessages] = useState([]);
+
+    useEffect(() => {
+        const unsubscribe = db
+            .collection('chats')
+            .doc(id)
+            .collection('messages')
+            .orderBy('timestamp', 'desc')
+            .onSnapshot(snapshot => {
+                setMessages(snapshot.docs.map(doc => doc.data()))
+            });
+
+        return unsubscribe
+    }, [])
+
     return (
-        <ListItem onPress={() => enterChat(id, chatName)}>
-            <Avatar rounded source={{ uri: `https://cencup.com/wp-content/uploads/2019/07/avatar-placeholder.png` }}/>
+        <ListItem key={id} onPress={() => enterChat(id, chatName)}>
+            <Avatar rounded source={{ uri: messages?.[0]?.photoURL || `https://cencup.com/wp-content/uploads/2019/07/avatar-placeholder.png` }}/>
 
             <ListItem.Content>
                 <ListItem.Title style={{ fontWeight: "800" }}>{chatName}</ListItem.Title>
                 <ListItem.Subtitle numberOfLines={1} ellipsizeMode="tail"> {/* Truncates Text to 1 line */}
-                    Youtube                
+                    {messages?.[0]?.displayName}{messages?.[0]?.message ? `:` : ``} {messages?.[0]?.message}             
                 </ListItem.Subtitle>
             </ListItem.Content>
         </ListItem>
